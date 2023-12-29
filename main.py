59aa4846e764993ddb6612f0631bf8d30bcf5f88
Maialinux <133462458+Maialinux@users.noreply.github.com>
2023-12-29 01:04:23 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import qrcode
-from tkinter import messagebox, Tk, Label, Entry, Button
+import qrcode, customtkinter
+from tkinter import messagebox, PhotoImage
 
 # Bibliotecas(Módulos) instaladas:>>>> qrcode, pillow 
+
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("dark-blue")
 
 def gera_qr_code():
     url = website_entry.get()
@@ -27,19 +30,29 @@ def gera_qr_code():
 
 
 if __name__ == '__main__':
-    window = Tk()
+    window = customtkinter.CTk()
+    img = PhotoImage(file="./favicon.png")
+    window.iconphoto(True,img)
     window.title("Gerador de Código QR")
-    window.config(padx=10, pady=100)
+    window.geometry("600x400")
+    window.resizable(width=False, height=False)
+    window.config(padx=10, pady=20)
+    
 
     # Labels
-    website_label = Label(text="URL:")
-    website_label.grid(row=2, column=0)
+    website_label_titulo = customtkinter.CTkLabel(window,text="Gerador de QR-Code", font=("FFF Tusj", 50))
+    website_label_titulo.grid(row=0, column=1,pady=40)
+    website_label = customtkinter.CTkLabel(window,text="URL:")
+    website_label.grid(row=2, column=0,pady=20)
 
     # Entries
-    website_entry = Entry(width=35)
-    website_entry.grid(row=2, column=1, columnspan=2)
+    website_entry = customtkinter.CTkEntry(window,width=500)
+    website_entry.grid(row=2, column=1, columnspan=2,padx=20)
     website_entry.focus()
-    add_button = Button(text="Gerar QR Code", width=36, command=gera_qr_code)
+    add_button = customtkinter.CTkButton(window,text="Gerar QR Code", width=100, command=gera_qr_code)
     add_button.grid(row=4, column=1, columnspan=2,pady=20)
-
+    
+    # Rodapé
+    website_label_rodape = customtkinter.CTkLabel(window,text="Criado por Eduardo Maia <distromaialinux@gmail.com>", font=("Oswald", 22))
+    website_label_rodape.grid(row=5, column=1,pady=60)
     window.mainloop()
